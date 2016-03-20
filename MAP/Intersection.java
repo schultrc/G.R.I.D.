35ff29b66157ed009f937642a4fa4e7514992dbc
Ray <Ray@192.168.0.113>
2016-03-19 22:38:51 -0600

    Initial test setup. See if this works for you

---
+++ MAP/Intersection.java
@@ -0,0 +1,5 @@
+package MAP;
+
+public class Intersection {
+
+}
---
+++ MAP/Road.java
@@ -0,0 +1,15 @@
+package MAP;
+
+public class Road {
+	Intersection from;
+	Intersection to;
+	
+	// Defined in meters
+	double Length;
+	
+	// Defined in km/hr
+	double maxSpeed;
+
+	// Defined in km/hr
+	double currentSpeed;
+}
